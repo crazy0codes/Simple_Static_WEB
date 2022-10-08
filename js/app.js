a072feb27bb38a09b59baf750e2fef95a62bccd1
crazy0codes <madhanpanja@gmail.com>
2022-10-08 23:15:09 +0530

    change

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
-function Car(props) {
-    return <h2>I am a {props.color} Car!</h2>;
-  }
-  
-  const root = ReactDOM.createRoot(document.getElementById('demo'));
-  root.render(<Car color="red"/>);
+let Add = () => {
+    return(
+        <div>
+            <h1>Creating Notes Using React</h1>
+            <textarea className="form-control"></textarea>
+            <button className="btn btn-success">success</button>
+        </div>
+    )
+}
+ReactDOM.render(
+    <Add/>,
+    document.getElementById('demo'),
+    <Add/>,
+    document.getElementById('Name')
+)
